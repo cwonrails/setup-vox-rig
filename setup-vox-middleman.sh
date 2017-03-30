1539d66bb4acf179d2301a0b630fe44a09a693cf
Chris Watson <cwonrails@gmail.com>
2017-03-30 17:42:03 -0400

    chore: change shebang to increase portability

--- setup-vox-middleman.sh
+++ setup-vox-middleman.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#
+#!usr/bin/env bash
+
 # To run this script, enter this into your terminal:
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/voxmedia/setup-vox-rig/master/setup-vox-middleman.sh)"
 set -e
